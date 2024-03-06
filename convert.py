d4fd0d3a2896e0fc48ebff40fd67d2c0a77f79aa
Vincent Levasseur <vince.levasseur@gmail.com>
2024-03-06 18:00:48 +0100

    Add SYN VGS

--- convert.py
+++ convert.py
@@ -135,12 +135,14 @@ if __name__ == "__main__":
         "SY_2_SYN",
         "SY_2_AOD",
         "SY_2_VGP",
-        "SY_2_VGK"]
-    PRODUCTS_TO_PROCESSED = ["SY_2_VGP","SY_2_VGK"]
+        "SY_2_VGK",
+        "SY_2_VG1",
+        "SY_2_V10"]
+    PRODUCTS_TO_PROCESSED = ["SY_2_VG1"]
     for p in PRODUCTS_TO_PROCESSED:
         use_custom_simpl_mapping = False
         # In the cases of OLCI L1, lat/lon are duplicated in the /conditions/image_grid and /quality/image groups
-        if p in ["OL_1_EFR","OL_1_ERR","SY_2_AOD","SY_2_VGP","SY_2_VGK"]:
+        if p in ["OL_1_EFR","OL_1_ERR","SY_2_AOD","SY_2_VGP","SY_2_VGK","SY_2_VG1","SY_2_V10"]:
             use_custom_simpl_mapping = True
         print(f" ===== Convert {p} product, using custom simpl. mapping={use_custom_simpl_mapping}") 
         product_converter(
